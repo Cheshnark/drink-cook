4022a374dfd1c2ffe8ef22f559d98a951b6049fb
csnark <csnark.dev@gmail.com>
2022-10-18 16:46:32 +0200

    Beer(mobile) component completed

--- src/components/Beer/Beer.js
+++ src/components/Beer/Beer.js
@@ -21,12 +21,36 @@ const Beer = (props) => {
 
     return(
         <div className="beer">
-            <div className="item">
+            <div className="beer-item">
                 <div className="beer-img-container">
                     <img src={beer.image_url} alt='beer-img'/>
                 </div>
-                <h1>{beer.name}</h1>
-                <h2>{beer.tagline}</h2>
+                <div className="content-container">
+                    <h1>{beer.name}</h1>
+                    <h2>{beer.tagline}</h2>
+                    <div className="border-2 border-black"></div>
+                    <h3>DESCRIPTION</h3>
+                    <p>{beer.description}</p>
+                    <div className="specs-grid">
+                        <div>
+                            <h4>ALCOHOL</h4>
+                            <p>{beer.abv}%</p>
+                        </div>
+                        <div>
+                            <h4>IBU</h4>
+                            <p>{beer.ibu}</p>
+                        </div>
+                    </div>
+                    <h3>FOOD PAIRING</h3>
+                    <ul>
+                        <li>{beer.food_pairing[0]}</li>
+                        <li>{beer.food_pairing[1]}</li>
+                        <li>{beer.food_pairing[2]}</li>
+                    </ul>
+                    <h3>BREWER TIPS</h3>
+                    <p>{beer.brewers_tips}</p>
+                </div>
+
 
             </div>
         </div>
