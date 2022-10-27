9345ca987cc9772ae5676eba73b1f13cb55f1c4b
csnark <csnark.dev@gmail.com>
2022-10-27 18:59:18 +0200

    useBeerFetch finished and working

--- src/components/Beer/Beer.js
+++ src/components/Beer/Beer.js
@@ -1,81 +1,72 @@
 import './Beer.css'
-import { useEffect, useState } from "react";
+import useBeerFetch from '../useBeerFetch';
 
 const Beer = (props) => {
     const beerId = props.beerId;
-    const [beer, setBeer] = useState([]); 
+    const {beer, pending, error} = useBeerFetch('https://api.punkapi.com/v2/beers/' + beerId);
 
-    useEffect( () => {
-        fetch('https://api.punkapi.com/v2/beers/' + beerId)
-        .then(response => response.json())
-        .then( (data) => {
-            console.log(data);
-            setBeer(data[0]);
-        })
-        .catch((err) => {
-            console.log(err.message);
-        })
-    }, []);
 
-    const clickedBeer = () => {
-        const clickedBeer = beer.id;
-        console.log(clickedBeer);
-    };
 
 // What follows is a fix I arrived with to avoid the app from bugging when accessing the array, for it
 // considers is empty otherwise and the whole component doesn't load propertly. Need someone to check this. 
 
-    const foodPairing = beer.food_pairing;
-    let list = [];
+    // const foodPairing = beer.food_pairing;
+    // let list = [];
 
-    if(foodPairing === undefined) {
-        console.log('Fuck dis shit');
-    } else {
-        list = foodPairing
-    }
+    // if(foodPairing === undefined) {
+    //     console.log('Fuck dis shit');
+    // } else {
+    //     list = foodPairing
+    // }
+
+    console.log(beer);
 
     return(
         <div className="beer">
-            <div className="beer-item">
-                <div className="beer-x-container">
-                    <img src="images/x.png" className='beer-x-symbol hover:cursor-pointer' alt='x-symbol'/>
-                </div>
-                <div className="beer-img-container">
-                    <img src={beer.image_url} alt='beer-img'/>
-                </div>
-                <div className="content-container">
-                    <h1>{beer.name}</h1>
-                    <h2>{beer.tagline}</h2>
-                    <div className="border-2 border-black"></div>
-                    <h3>DESCRIPTION</h3>
-                    <p>{beer.description}</p>
-                    <div className="specs-grid">
-                        <div>
-                            <h4>ALCOHOL</h4>
-                            <p>{beer.abv}%</p>
-                        </div>
-                        <div>
-                            <h4>IBU</h4>
-                            <p>{beer.ibu}</p>
-                        </div>
+            {pending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {beer && (
+                <div className="beer-item">
+                    <div className="beer-x-container">
+                        <img src="images/x.png" className='beer-x-symbol hover:cursor-pointer' alt='x-symbol'/>
                     </div>
-                    <h3>FOOD PAIRING</h3>
-                    <div className="food-grid">
-                        <div className="food-list">
-                        <ul>
-                            <li>{list[0]}</li>
-                            <li>{list[1]}</li>
-                            <li>{list[2]}</li>
-                        </ul>
-                        </div>
-                        <div className="food-buttons">
-                        <button id={beer.id} onClick={clickedBeer} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Random cooking?</button>
-                        </div>
+                    <div className="beer-img-container">
+                        <img src={beer[0].image_url} alt='beer-img'/>
                     </div>
-                    <h3>BREWER TIPS</h3>
-                    <p>{beer.brewers_tips}</p>
+                    <div className="content-container">
+                        <h1>{beer[0].name}</h1>
+                        <h2>{beer[0].tagline}</h2>
+                        <div className="border-2 border-black"></div>
+                        <h3>DESCRIPTION</h3>
+                        <p>{beer[0].description}</p>
+                        <div className="specs-grid">
+                            <div>
+                                <h4>ALCOHOL</h4>
+                                <p>{beer[0].abv}%</p>
+                            </div>
+                            <div>
+                                <h4>IBU</h4>
+                                <p>{beer[0].ibu}</p>
+                            </div>
+                        </div>
+                        <h3>FOOD PAIRING</h3>
+                        <div className="food-grid">
+                            <div className="food-list">
+                            <ul>
+                                <li>{beer[0].food_pairing[0]}</li>
+                                <li>{beer[0].food_pairing[1]}</li>
+                                <li>{beer[0].food_pairing[2]}</li>
+                            </ul>
+                            </div>
+                            <div className="food-buttons">
+                            <button id={beer[0].id} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Random cooking?</button>
+                            </div>
+                        </div>
+                        <h3>BREWER TIPS</h3>
+                        <p>{beer[0].brewers_tips}</p>
+                    </div>
                 </div>
-            </div>
+            )}
         </div>
     )
 }
--- src/pages/Beers/Beers.js
+++ src/pages/Beers/Beers.js
@@ -1,28 +1,14 @@
 import './Beers.css';
 import Navbar from '../../components/NavBar/NavBar.js';
 import Beer from '../../components/Beer/Beer';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import useBeerFetch from '../../components/useBeerFetch';
 
 const Beers = () => {
 
-    const [beers, setBeers] = useState([]);
     const [beerId, setBeerId] = useState([]);
     const [showBeer, setShowBeer] = useState(false);
-
-    useEffect( () => {
-        fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
-        .then(response => response.json())
-        .then( (data) => {
-            console.log(data);
-            setBeers(data);
-        })
-        .catch((err) => {
-            console.log(err.message);
-        })
-
-        //Desactiva una regla de useEffect que salta cuando se deja la dependencia vacía. 
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const {beer:beers, pending, error} = useBeerFetch('https://api.punkapi.com/v2/beers?page=2&per_page=80');
 
     const beerInfo = (e) => {
         setBeerId(e.target.id);
@@ -32,42 +18,47 @@ const Beers = () => {
     return (
         <div className="beers">
             <Navbar />
-            <div className='title-1'>
-                <h1>BEERS</h1>
-            </div>
-            <div className="beers-container">
-                {beers.map((beer, index) => (
-                    <div className="item" key={index}>
-                        <div className="beers-img-container">
-                            <img src={beer.image_url} alt='beers-img'/>
-                        </div>
-                        <h2>{beer.name}</h2>
-                        <p>{beer.tagline}</p>
-                        <div className="beer-buttons">
-                            <button id={beer.id} onClick={beerInfo} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Info</button>
-                            <button id={beer.id} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">What to cook?</button>
-                        </div>
+            {pending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {beers && (
+                <div>
+                    <div className='title-1'>
+                        <h1>BEERS</h1>
                     </div>
-                ))}               
-            </div>
-            
-            {showBeer && ( 
-            <div className="beer-container" onClick={ () => setShowBeer(false)}>
-                <Beer beerId={beerId} />
-            </div> )}
-
-            {/* <div className="item">
-                <div className="beers-img-container">
-                    <img src="https://images.punkapi.com/v2/132.png"/>
+                    <div className="beers-container">
+                        {beers.map((beer, index) => (
+                            <div className="item" key={index}>
+                                <div className="beers-img-container">
+                                    <img src={beer.image_url} alt='beers-img'/>
+                                </div>
+                                <h2>{beer.name}</h2>
+                                <p>{beer.tagline}</p>
+                                <div className="beer-buttons">
+                                    <button id={beer.id} onClick={beerInfo} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Info</button>
+                                    <button id={beer.id} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">What to cook?</button>
+                                </div>
+                            </div>
+                        ))}               
+                    </div>
+                        
+                    {showBeer && ( 
+                    <div className="beer-container" onClick={ () => setShowBeer(false)}>
+                        <Beer beerId={beerId} />
+                    </div> )}
+                    
+                    {/* <div className="item">
+                        <div className="beers-img-container">
+                            <img src="https://images.punkapi.com/v2/132.png"/>
+                        </div>
+                        <h2>Peroxide Punk</h2>
+                        <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
+                        <div className="beer-buttons">
+                            <button onClick={beerInfo} id='197' class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Info</button>
+                            <button onClick={beerDish} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">What to cook?</button>
+                        </div>
+                    </div> */}
                 </div>
-                <h2>Peroxide Punk</h2>
-                <p>Post Modern Classic. Spiky. Tropical. Hoppy.</p>
-                <div className="beer-buttons">
-                    <button onClick={beerInfo} id='197' class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">Info</button>
-                    <button onClick={beerDish} class="bg-transparent hover:bg-orange-600 text-orange-700 font-semibold hover:text-white py-2 px-4 border border-orange-500 hover:border-transparent rounded">What to cook?</button>
-                </div>
-            </div> */}
-
+            )}
         </div>
         
     )
